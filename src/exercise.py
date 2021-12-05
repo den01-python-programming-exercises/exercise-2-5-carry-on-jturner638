3b25d4c737208e04652616067bdfc7c59c593876
Justin Turner <mrfathed@protonmail.com>
2021-12-05 09:22:57 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    while True:
+        response = input("Carry on?")
+
+        if response == "no":
+            break
 
 if __name__ == '__main__':
     main()
